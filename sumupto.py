280b232467a4de5f10ec10b1af3d91cd01e699cf
Clodagh Murphy <clodaghspud808@gmail.com>
2019-02-09 19:32:46 +0000

    First Draft of problem 1

---
+++ sumupto.py
@@ -0,0 +1,19 @@
+#python sumupto
+#Write a program that asks the user to input any positive integer 
+#and outputs the sum of all numbers between one and that number. 
+#Clodagh Murphy 2019 01 29
+
+#firstly ask the user to input a positive integer i.e. a whole number.
+
+i = int(input("Hello. Please input a positive whole number. "))
+
+input
+if i <= 0:
+    print("I require a positive whole number. Please run the program again.")
+# is the answer to use the Gauss formula?
+# n(n+1)/2
+
+x= int (i*(i+1))//2
+print (x)
+
+      
