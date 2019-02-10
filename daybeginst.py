c2783e6daf4a3f7c4d542b862a0d09e0b6cb3fd9
Clodagh Murphy <clodaghspud808@gmail.com>
2019-02-10 18:00:26 +0000

    Second Version of daybeginst

--- daybeginst.py
+++ daybeginst.py
@@ -2,16 +2,20 @@
 #Write a program that outputs whether or not today is a day that begins with the letter T
 #Clodagh Murphy 2019 02 10
 
-
+#Use the import statement to gain access to datetime Python code from another module
 import datetime
 
+#Assign the day of the week to x
+x = (datetime.datetime.today().weekday())
+#Returns the day of the week as an integer, where Monday is 0 and Sunday is 6.
+#Therefore Tuesday is 1 and Thursday is 3
 
-if datetime.datetime.today().weekday() == 2:
-  print("Yes - today begins with a T.")
-else:
-  print("No - today does not begin with a T.")
 
-if datetime.datetime.today().weekday() == 4:
-  print("Yes - today begins with a T.")
-else:
-  print("No - today does not begin with a T.")
+if x == 1: 
+  print("Yes - today begins with a T.")  
+if x == 3: 
+  print("Yes - today begins with a T.") 
+# "else" is an optional statement that executes 
+# when the conditional expression in the if statement above supplies a FALSE value
+else: 
+  print ("No - today does not begin with a T") 
