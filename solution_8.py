a8c7ec5f8165d32872acd6f6cd8f40b6a4cb2ec1
Clodagh Murphy <clodaghspud808@gmail.com>
2019-03-10 10:45:53 +0000

    10

--- solution_8.py
+++ solution_8.py
@@ -2,7 +2,7 @@
 # the format "Monday,January 10th 2019 at 1:15pm”. 
 #Clodagh Murphy 08 03 2019
 #First import datetime module in order to access it's functionality
-import datetime
+#import datetime
 #Print the current date and time
 #Use strftime function to indicate the particular format the date and time should take
 #%A Weekday as locale’s full name
@@ -11,14 +11,31 @@ import datetime
 #%Y Year with century as a decimal number.
 #%I:%M  Hour:Minute (12-hour clock) as a zero-padded decimal number.
 #%p Locale’s equivalent of either AM or PM.
-print(datetime.datetime.now().strftime("%A, %B %d %Y at %I:%M %p"))
+#print(datetime.datetime.now().strftime("%A, %B %d %Y at %I:%M %p"))
+#Above command produces result in following format
+#Sunday, March 10 2019 at 09:29 AM
+#The solution requires English ordinal suffix for the day of the month i.e. 10th
+#this is not a functionality of python
+#I adapted a piece of code that provides a suffix by user gsteff from stackoverflow
+#https://stackoverflow.com/questions/5891555/display-the-date-like-may-5th-using-pythons-strftime/5891658#5891658
+#It uses the time module
+# and The localtime() function which returns struct_time in local time.
+import time
+import datetime
 
+day_endings = {
+    1: 'st',
+    2: 'nd',
+    3: 'rd',
+    21: 'st',
+    22: 'nd',
+    23: 'rd',
+    31: 'st'}
 
-#CODE INCOMPLETE I NEED TO CREATE A FUNCTION WITH THE FOLLOWING BIT
-#OF CODE TO SHOW THE DATE WITH A SUFFIX
-
-#https://bytes.com/topic/python/answers/155606-time-formatting-date-suffixes
-#if 4 <= day <= 20 or 24 <= day <= 30:
-#suffix = "th"
-#else:
-#suffix = ["st", "nd", "rd"][day % 10 - 1]
+def custom_strftime (format, t):
+    return time.strftime(format, t).replace('{TH}', str(t[2]) + day_endings.get(t[2], 'th'))
+#return time.strftime(format, t).replace('{TH}', str(t[2]) + day_endings.get(t[2], 'th'))
+#print(datetime.datetime.now().strftime("%A, %B %d %Y at %I:%M %p"))
+#print (custom_strftime("%A, %B {TH}, %Y", time.localtime()))
+print (custom_strftime("%A, %B {TH} %Y at %I:%M %p", time.localtime()))
+#print (custom_strftime("%A, %B {TH} %Y at %I:%M %p", datetime.datetime.now()))
