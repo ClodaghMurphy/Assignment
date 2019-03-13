3864f8d6890530837e91d5fa79cf66f3856673c9
Clodagh Murphy <clodaghspud808@gmail.com>
2019-03-13 19:37:02 +0000

    13March20hrs

--- solution_4.py
+++ solution_4.py
@@ -10,25 +10,24 @@
 
 i = int(input("Hello. Please input a positive whole number. "))
 # Tell the user it must be a positive integer if it is not one
-input
+
 while i <= 0:
     print("I require a positive whole number. Please run the program again.")
     exit()
 # Use the exit() method to stop the program running. 
 # It is the most reliable, cross-platform way of stopping code execution
 
-#total = 0
-#while i > 0
-#    total = total + i
+while i != 1:
+#!= means not equal to, therefore when i is equal to 1 the following loop won't happen
+    if i%2 == 0: print(i//2)
+#no need to write code for both i%2==0 and i%2==1 
+#If one is true, the other is automatically false
+# Boolean innit?
+    else:
+        print(i*3+1)
 
-
-while i > 1:
-    if i%2==0:
-        i = (i//2)
-    print (i)
-    #elif i%2==1:
-      #  i = (i*3+1)
-print (i)
-#else: 
-    #print ("OK")
-
+#13032019 as this code currently stands the loop isn't happening as desired.
+#i need to make the loop a function as per week 6 programming video
+#then feed the input into it
+#as illustrated by user R2DPoop
+#https://stackoverflow.com/questions/33324432/collatz-sequence-python-3
