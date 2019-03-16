a105f1aeb54016fbc4cb716b65a7699b37fd8987
Clodagh Murphy <clodaghspud808@gmail.com>
2019-03-16 11:19:34 +0000

    16032019b

--- solution_6.py
+++ solution_6.py
@@ -32,16 +32,32 @@ print( fruitlist )
 
 #You can delete elements from a list 
 #by assigning an empty list to a slice[].
-s = (input("Hello. Please input a short sentence of 10 words or less. "))
-t = s.split()
-t[0] = [] 
-t[2] = [] 
-t[4] = [] 
-t[6] = [] 
-t[8] = [] 
+#s = (input("Hello. Please input a short sentence of 10 words or less. "))
+#t = s.split()
+#t[0] = [] 
+#t[2] = [] 
+#t[4] = [] 
+#t[6] = [] 
+#t[8] = [] 
 
-print (t)
+#print (t)
 #This code kinda works but returns IndexError: list assignment index out of range
 # if the input is less than the #number of elements in the code, if there isn't an eighth
 #element for example, needs to be more conditonal
 #Also the output is 'ugly'
+
+s = (input("Hello. Please input a sentence of 11 words. ")) 
+fruitlist = s.split()
+#del is a keyword that allows you to delete a list element, or list slice, by index.
+#every time an element is deleted, the code runs again and the element's location on the list
+#changes
+#Again this is code only works on a list with 11 words and output is ugly
+#not much use!
+del fruitlist[0] 
+del fruitlist[1] 
+del fruitlist[2] 
+del fruitlist[3] 
+del fruitlist[4] 
+del fruitlist[5]
+
+print( fruitlist )
