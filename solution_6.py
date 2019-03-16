3d48e8b207af1215987baa8c822bf87e094233e5
Clodagh Murphy <clodaghspud808@gmail.com>
2019-03-16 19:00:57 +0000

    16032019pm

--- solution_6.py
+++ solution_6.py
@@ -3,10 +3,10 @@
 
 #Preparation work using exercises from How to Think Like a Computer Scientist by Allen Downey
 
-t = ['a', 'b', 'c']
-x = t.pop(1)
-print (t)
-print (x)
+#t = ['a', 'b', 'c']
+#x = t.pop(1)
+#print (t)
+#print (x)
 
 #pop modifies the list and returns the element that was removed. If you don’t provide an
 #index, it deletes and returns the last element.
@@ -17,18 +17,18 @@ print (x)
 #print (x)
 #result TypeError: pop() takes at most 1 argument (4 given)
 
-t = ['i', 'would', 'like', 'to', 'go', 'for', 'a', 'walk']
-del t[1:5]
-print (t)
+#t = ['i', 'would', 'like', 'to', 'go', 'for', 'a', 'walk']
+#del t[1:5]
+#print (t)
 
-s = 'pining for the fjords'
-t = s.split()
-print (t)
+#s = 'pining for the fjords'
+#t = s.split()
+#print (t)
 
-fruitlist = ["apple", "banana", "cherry", "durian", "orange"]
-print( fruitlist ) 
-fruitlist[1:3] = [] 
-print( fruitlist )
+#fruitlist = ["apple", "banana", "cherry", "durian", "orange"]
+#print( fruitlist ) 
+#fruitlist[1:3] = [] 
+#print( fruitlist )
 
 #You can delete elements from a list 
 #by assigning an empty list to a slice[].
@@ -46,18 +46,49 @@ print( fruitlist )
 #element for example, needs to be more conditonal
 #Also the output is 'ugly'
 
-s = (input("Hello. Please input a sentence of 11 words. ")) 
-fruitlist = s.split()
+#s = (input("Hello. Please input a sentence of 11 words. ")) 
+#fruitlist = s.split()
 #del is a keyword that allows you to delete a list element, or list slice, by index.
 #every time an element is deleted, the code runs again and the element's location on the list
 #changes
 #Again this is code only works on a list with 11 words and output is ugly
 #not much use!
-del fruitlist[0] 
-del fruitlist[1] 
-del fruitlist[2] 
-del fruitlist[3] 
-del fruitlist[4] 
-del fruitlist[5]
+#del fruitlist[0] 
+#del fruitlist[1] 
+#del fruitlist[2] 
+#del fruitlist[3] 
+#del fruitlist[4] 
+#del fruitlist[5]
 
-print( fruitlist )
+#print( fruitlist )
+#print (word[2])       #get one char of the word
+#print (word[0:1]   )     #get one char of the word (same as above)
+#print (word[0:3] )       #get the first three char
+#print (word[:3]  )       #get the first three char
+#print (word[-3:]  )      #get the last three char
+#print (word[3:]  )       #get all but the three first char
+#print (word[:-3]  )      #get all but the three last character
+
+#phrase = input("Enter a phrase: ")
+#words = phrase.split()  # ['I', 'need', 'practice']
+#for word in words:
+#  print(word)
+
+#phrase=input('enter a phrase: ')
+#for char in phrase:
+ #   print (char, end ='')
+    #words = phrase.replace(' ', '\n')
+#print(words)
+
+
+#Solution adapted from contribution by user Merig
+#https://stackoverflow.com/questions/54857129/
+#write-a-program-that-takes-a-user-input-string-and-outputs-every-second-word/54857192#54857192
+words = input("Enter a phrase: ")
+#All data from the input command is stored as a string variable.
+#Use the split method to break a string into words
+words = words.split()
+#the [::2] slice syntax will return items from the array going up by two
+print (words[::2]) #this command outputs words 0,2,4 etc starting fom 0 not 1
+#not every second word as the human eye counts
+#but that is how the example provided for the problem appears so it must be grand
